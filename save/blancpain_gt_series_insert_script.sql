af80fac48bf923f0c9f8da0cb0548ca8fd40f0c2
Ivan Harasta <harasta.ivan@gmail.com>
2018-12-10 22:41:23 +0100

    Add more drivers to make more driver groups

--- save/blancpain_gt_series_insert_script.sql
+++ save/blancpain_gt_series_insert_script.sql
@@ -1,3 +1,5 @@
+delete from RESULTS;
+delete from PARTICIPATION;
 delete from RACE;
 delete from QUALIFYING;
 delete from "Session";
@@ -12,6 +14,7 @@ delete from DRIVER_GROUP;
 delete from RACECAR;
 delete from CIRCUIT;
 delete from TEAM;
+commit;
 
 -- Team
 insert into TEAM (team_id, foundation_date, name, website_url) values (1, '25.03.2002', 'AF Corse', 'afcorse.it');
@@ -40,25 +43,49 @@ insert into RACECAR (racecar_id, manufacturer, type, full_name, "number", class,
 commit;
 
 -- Driver_group
-insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (1, 3);
-insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (2, 2);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (1, 1);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (2, 1);
 insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (3, 1);
-insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (4, 4);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (4, 2);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (5, 2);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (6, 2);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (7, 3);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (8, 3);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (9, 3);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (10, 4);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (11, 4);
+insert into DRIVER_GROUP (DRIVER_GROUP_ID, RACECAR_RACECAR_ID) values (12, 4);
 commit;
 
 -- Driver
 insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (1, 'Nicki', 'Thiim', '17.04.1989', 0, 'DK', 'danskyjr.com');
-insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (2, 'Rubens', 'Barrichello', '22.06.1980', 0, 'IT', 'barrichello.com');
-insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (3, 'Rene', 'Rast', '11.03.1985', 0, 'DE', 'rast.com');
-insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (4, 'Jake', 'Dennis', '11.04.1988', 0, 'GB', null);
+insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (2, 'Jake', 'Dennis', '11.04.1988', 0, 'GB', null);
+insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (3, 'Rubens', 'Barrichello', '22.06.1980', 0, 'IT', 'barrichello.com');
+insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (4, 'Rubens2', 'Barrichello2', '22.06.1980', 0, 'IT', 'barrichello.com');
+insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (5, 'Rene', 'Rast', '11.03.1985', 0, 'DE', 'rast.com');
+insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (6, 'Rene2', 'Rast2', '11.03.1985', 0, 'DE', 'rast.com');
+insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (7, 'Alex', 'Riberas', '11.03.1985', 0, 'DE', 'riberas.com');
+insert into DRIVER (driver_id, first_name, last_name, birth_date, gender, nationality, website_url) values (8, 'Alex2', 'Riberas2', '11.03.1985', 0, 'DE', 'riberas.com');
+
 commit;
 
 -- Driver_driver_group
 insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (1, 1);
-insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (1, 2);
-insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (4, 2);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (2, 2);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (1, 3);
 insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (2, 3);
 insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (3, 4);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (4, 5);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (3, 6);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (4, 6);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (5, 7);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (6, 8);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (5, 9);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (6, 9);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (7, 10);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (8, 11);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (7, 12);
+insert into DRIVER_DRIVER_GROUP (DRIVER_DRIVER_ID, DRIVER_GROUP_DRIVER_GROUP_ID) VALUES (8, 12);
 commit;
 
 -- Sponsor
@@ -109,12 +136,28 @@ commit;
 -- Session
 insert into "Session" (SESSION_ID, NAME, BEGIN_DATE, END_DATE, CIRCUIT_CIRCUIT_ID, SESSION_TYPE) VALUES (1, 'Spa 24 Hour Qualifying', to_date('15.6.2017 14:00', 'dd.mm.yyyy HH24:mi'), to_date('15.6.2017 16:00', 'dd.mm.yyyy HH24:mi'), 7, 'Qualifying');
 insert into "Session" (SESSION_ID, NAME, BEGIN_DATE, END_DATE, CIRCUIT_CIRCUIT_ID, SESSION_TYPE) VALUES (2, 'Spa 24 Hour Main Race', to_date('16.6.2017 15:00', 'dd.mm.yyyy HH24:mi'), to_date('17.6.2017 15:00', 'dd.mm.yyyy HH24:mi'), 7, 'Race');
+insert into "Session" (SESSION_ID, NAME, BEGIN_DATE, END_DATE, CIRCUIT_CIRCUIT_ID, SESSION_TYPE) VALUES (3, 'Misano Qualifying', to_date('22.3.2017 12:00', 'dd.mm.yyyy HH24:mi'), to_date('22.3.2017 14:00', 'dd.mm.yyyy HH24:mi'), 7, 'Qualifying');
+insert into "Session" (SESSION_ID, NAME, BEGIN_DATE, END_DATE, CIRCUIT_CIRCUIT_ID, SESSION_TYPE) VALUES (4, 'Misano Sprint Race', to_date('23.3.2017 15:00', 'dd.mm.yyyy HH24:mi'), to_date('23.3.2017 18:00', 'dd.mm.yyyy HH24:mi'), 7, 'Race');
 commit;
 
 -- Qualifying
 insert into QUALIFYING (SESSION_ID, TRUE_DURATION) VALUES (1, 100);
+insert into QUALIFYING (SESSION_ID, TRUE_DURATION) VALUES (3, 60);
 commit;
 
 -- Race
-insert into RACE (SESSION_ID, FORMATION_LAP, LAPS) VALUES (2, 1, 250);
+insert into RACE (SESSION_ID, FORMATION_LAP, LAPS) VALUES (2, 1, 626);
+insert into RACE (SESSION_ID, FORMATION_LAP, LAPS) VALUES (4, 1, 102);
+commit;
+
+insert into PARTICIPATION (PARTICIPATION_ID, DRIVER_GROUP_DRIVER_GROUP_ID, QUALIFYING_SESSION_ID, RACE_SESSION_ID, REGISTRATION_DATE) VALUES (1, 3, 1, 2, '1.5.2017');
+insert into PARTICIPATION (PARTICIPATION_ID, DRIVER_GROUP_DRIVER_GROUP_ID, QUALIFYING_SESSION_ID, RACE_SESSION_ID, REGISTRATION_DATE) VALUES (2, 6, 1, 2, '1.5.2017');
+insert into PARTICIPATION (PARTICIPATION_ID, DRIVER_GROUP_DRIVER_GROUP_ID, QUALIFYING_SESSION_ID, RACE_SESSION_ID, REGISTRATION_DATE) VALUES (3, 9, 1, 2, '1.5.2017');
+insert into PARTICIPATION (PARTICIPATION_ID, DRIVER_GROUP_DRIVER_GROUP_ID, QUALIFYING_SESSION_ID, RACE_SESSION_ID, REGISTRATION_DATE) VALUES (4, 12, 1, 2, '1.5.2017');
+commit;
+
+insert into RESULTS (BEST_QUALIFYING_LAPTIME, STARTING_POSITION, PITS_COUNT, FINAL_POSITION, POINTS, INCIDENT_COUNT, PARTICIPATION_PARTICIPATION_ID) VALUES ('2:20.543', 2, 12, 1, 500, 10, 1);
+insert into RESULTS (BEST_QUALIFYING_LAPTIME, STARTING_POSITION, PITS_COUNT, FINAL_POSITION, POINTS, INCIDENT_COUNT, PARTICIPATION_PARTICIPATION_ID) VALUES ('2:20.152', 3, 14, 2, 400, 10, 4);
+insert into RESULTS (BEST_QUALIFYING_LAPTIME, STARTING_POSITION, PITS_COUNT, FINAL_POSITION, POINTS, INCIDENT_COUNT, PARTICIPATION_PARTICIPATION_ID) VALUES ('2:21.002', 1, 13, 3, 300, 10, 2);
+insert into RESULTS (BEST_QUALIFYING_LAPTIME, STARTING_POSITION, PITS_COUNT, FINAL_POSITION, POINTS, INCIDENT_COUNT, PARTICIPATION_PARTICIPATION_ID) VALUES ('2:21.104', 4, 12, 4, 200, 10, 3);
 commit;
